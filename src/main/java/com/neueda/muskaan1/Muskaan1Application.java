fb79b368ddc7ded9876cd5970f1b1b53df6994c3
Diksha <dikshanegi2019@gmail.com>
2023-08-09 09:29:12 +0000

    spring initilize

---
+++ src/main/java/com/neueda/muskaan1/Muskaan1Application.java
@@ -0,0 +1,13 @@
+package com.neueda.muskaan1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Muskaan1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Muskaan1Application.class, args);
+	}
+
+}
