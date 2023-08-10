a6baba0c17b4a0fd0a78996b7625b0f436d20f30
godmodenorfin <kshubham1904@gmail.com>
2023-08-11 00:15:20 +0530

    fgyuhgvhjkjbvn

--- src/main/java/com/neueda/muskaan1/controller/CustomerController.java
+++ src/main/java/com/neueda/muskaan1/controller/CustomerController.java
@@ -5,71 +5,52 @@ import com.neueda.muskaan1.service.CustomerService;
 import com.neueda.muskaan1.exception.CustomerAlreadyExists;
 import com.neueda.muskaan1.exception.CustomerNotFound;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/customers")
 public class CustomerController {
 
     @Autowired
     private CustomerService customerService;
 
-    @GetMapping("/{customer}")
-    public List<Customer> getAllUsers() {return customerService.getAllCustomer();
+    @GetMapping
+    public List<Customer> getAllCustomers() {
+        return customerService.getAllCustomer();
     }
 
-
-    @PostMapping("/{customer}")
-    public Customer addCustomer(Customer customer) {
-//        if(customerService.getCustomerById(customer.getCustomer_id()))
-//            throw new CustomerAlreadyExists("User with "+ customer.getCustomer_id() + " already exists!");
-//        return (Customer) customerService.addCustomer(customer);
-//
-    try{
-        return this.customerService.addCustomer(customer);}
-    catch(CustomerAlreadyExists e){
-        System.out.println(e.getMessage());
-    }
-return null;
+    @PostMapping
+    public ResponseEntity<Customer> addCustomer(@RequestBody Customer customer) {
+        try {
+            Customer savedCustomer = customerService.addCustomer(customer);
+            return new ResponseEntity<>(savedCustomer, HttpStatus.CREATED);
+        } catch (CustomerAlreadyExists e) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
     }
 
     @GetMapping("/{customer_id}")
-    public String getUserById(@PathVariable String id){
-//        return customerService.getCustomerById(id).orElseThrow(() -> new CustomerNotFound("User with "+ "id"+id));
-//        Customer savedEntity=this.repo.save(c);
-//        return savedEntity;
-
-        try{
-            return this.customerService.getCustomerById(id);}
-        catch(CustomerNotFound e){
-//            System.out.println(e.getMessage());
-//            throw new CustomerNotFound(e.);
-           return this.customerService;
+    public ResponseEntity<Customer> getCustomerById(@PathVariable String customer_id) {
+        try {
+            Customer customer = customerService.getCustomerById(customer_id);
+            return new ResponseEntity<>(customer, HttpStatus.OK);
+        } catch (CustomerNotFound e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return null;
-
-
-    }
-//     if(repo.existsById(c.getCustomer_id())) throw new CustomerAlreadyExists("Customer with "+ c.getCustomer_id()+" already exists");
-
-
-    @PutMapping("/users/{id}")
-    public User updateUser(@PathVariable String id, @RequestBody User user) {
-        User existingUser = customerService.findById(id).orElse(null);
-        if (existingUser != null) {
-            existingUser.setName(user.getName());
-            existingUser.setEmail(user.getEmail());
-
-            return customerService.save(existingUser);
-        }
-        return null;
     }
 
-    @DeleteMapping("/users/{id}")
-    public void deleteUser (@PathVariable String id)throws CustomerNotFound {
-        if(!customerService.existsById(id))
-            throw new CustomerNotFound("User with "+ id + " not found!");
-        mongoRepository.deleteById(id);
+    @PutMapping("/{customer_id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable String customer_id, @RequestBody Customer customer) {
+        Customer updatedCustomer = customerService.updateCustomer(customer_id, customer);
+        return new ResponseEntity<>(updatedCustomer, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{customer_id}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable String customer_id) {
+        customerService.deleteCustomer(customer_id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
