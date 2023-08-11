165d6edd1555fb0cbef628854ff670c959ae65d8
Shubham Kumar <kshubham1904@gmail.com>
2023-08-11 13:50:24 +0530

    Update CustomerService.java

--- src/main/java/com/neueda/muskaan1/service/CustomerService.java
+++ src/main/java/com/neueda/muskaan1/service/CustomerService.java
@@ -1,12 +1,15 @@
+
 package com.neueda.muskaan1.service;
 
 import com.neueda.muskaan1.entity.Customer;
-import com.neueda.muskaan1.exception.*;
+import com.neueda.muskaan1.exception.CustomerAlreadyExists;
+import com.neueda.muskaan1.exception.CustomerNotFound;
 import com.neueda.muskaan1.repo.ICustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CustomerService {
@@ -14,49 +17,39 @@ public class CustomerService {
     @Autowired
     private ICustomerRepository repo;
 
-    public CustomerService(ICustomerRepository repo) {
-        this.repo = repo;
-    }
-    //methods
-
     public Customer addCustomer(Customer c) throws CustomerAlreadyExists {
+        if (repo.existsById(c.getCustomer_id())) {
+            throw new CustomerAlreadyExists("Customer with ID " + c.getCustomer_id() + " already exists");
+        }
+        return repo.save(c);
+    }
 
-     if(repo.existsById(c.getCustomer_id()))
-         throw new CustomerAlreadyExists("Customer with "+ c.getCustomer_id()+" already exists");
-        return this.repo.save(c);
-    }
-    //    public long getCount(){
-//        return this.repo.count();
-//    }
-    public List<Customer>getAllCustomer(){
-        return this.repo.findAll();
-    }
-    public Customer updateCustomer(String customer_id, Customer dataToUpdate) {
-        if (repo.existsById(dataToUpdate.getCustomer_id()))
-        {
-            repo.save((dataToUpdate));
-        }
-        else
-            System.out.println("Customer not found");
-        return dataToUpdate;
-    }
-    public void deleteCustomer(String id){
-        Customer c =repo.findById(id).orElse(null);
-        if(c!=null){
-            repo.save(c);
-        }
-        else
-            System.out.println("not found");
+    public List<Customer> getAllCustomer() {
+        return repo.findAll();
     }
 
     public Customer getCustomerById(String id) throws CustomerNotFound {
-        Customer customer = repo.findById(id).orElse(null);
-        if (customer == null) {
+        Optional<Customer> optionalCustomer = repo.findById(id);
+        if (optionalCustomer.isPresent()) {
+            return optionalCustomer.get();
+        } else {
             throw new CustomerNotFound("Customer with ID " + id + " not found");
         }
-        return customer;
     }
 
+    public void updateCustomer(Customer dataToUpdate) {
+        if (repo.existsById(dataToUpdate.getCustomer_id())) {
+            repo.save(dataToUpdate);
+        } else {
+            System.out.println("None found.");
+        }
+    }
 
-
-
+    public void deleteCustomer(String id) throws CustomerNotFound {
+        if (repo.existsById(id)) {
+            repo.deleteById(id);
+        } else {
+            throw new CustomerNotFound("Customer with ID " + id + " not found");
+        }
+    }
+}
