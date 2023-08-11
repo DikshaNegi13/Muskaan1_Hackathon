c52f5a152fb73139853fe9fdcdcdfe4d1a432425
Shubham Kumar <kshubham1904@gmail.com>
2023-08-11 13:52:03 +0530

    Update SpringBootWebApplication.java

--- src/main/java/com/neueda/muskaan1/SpringBootWebApplication.java
+++ src/main/java/com/neueda/muskaan1/SpringBootWebApplication.java
@@ -1,30 +1,14 @@
 package com.neueda.muskaan1;
 
-import com.neueda.muskaan1.repo.ICustomerRepository;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.logging.Logger;
-
-import static org.slf4j.LoggerFactory.getLogger;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-public class SpringBootWebApplication implements CommandLineRunner {
-	@Autowired
-	ICustomerRepository customerRepository;
-	static Logger logger= Logger.getLogger(SpringBootWebApplication.class.getSimpleName());
+@EnableSwagger2
+public class SpringBootWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootWebApplication.class, args);
-
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebApplication.class, args);
+    }
 }
