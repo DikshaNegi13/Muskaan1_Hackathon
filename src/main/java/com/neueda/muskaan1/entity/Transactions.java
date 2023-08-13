4998c02f3c96ef2cbd87f04f5d7f8307895cff91
Pratyaksha Goswami <goswamipratyaksha02@gmail.com>
2023-08-13 07:01:41 +0000

    updated

---
+++ src/main/java/com/neueda/muskaan1/entity/Transactions.java
@@ -0,0 +1,191 @@
+package com.neueda.muskaan1.entity;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document("transaction")
+
+public class Transactions {
+    public class Id{
+        public String $oid;}
+        public Id _id;
+
+    public Id get_id() {
+        return _id;
+    }
+
+    public void set_id(Id _id) {
+        this._id = _id;
+    }
+
+    public String getTrans_date_trans_time() {
+        return trans_date_trans_time;
+    }
+
+    public void setTrans_date_trans_time(String trans_date_trans_time) {
+        this.trans_date_trans_time = trans_date_trans_time;
+    }
+
+    public double getAmt() {
+        return amt;
+    }
+
+    public void setAmt(double amt) {
+        this.amt = amt;
+    }
+
+    public int getTrans_num() {
+        return trans_num;
+    }
+
+    public void setTrans_num(int trans_num) {
+        this.trans_num = trans_num;
+    }
+
+    public int getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(int customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getCity_population() {
+        return city_population;
+    }
+
+    public void setCity_population(int city_population) {
+        this.city_population = city_population;
+    }
+
+    public String getMerchant() {
+        return merchant;
+    }
+
+    public void setMerchant(String merchant) {
+        this.merchant = merchant;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public void setLast(String last) {
+        this.last = last;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String trans_date_trans_time;
+        public double amt;
+        public int trans_num;
+        public int customer_id;
+        public String city;
+        public String state;
+        public int city_population;
+        public String merchant;
+        public String category;
+        public String first;
+        public String last;
+
+    public Transactions(Id _id, String trans_date_trans_time, double amt, int trans_num, int customer_id, String city, String state, int city_population, String merchant, String category, String first, String last, String gender, String job, String dob) {
+        this._id = _id;
+        this.trans_date_trans_time = trans_date_trans_time;
+        this.amt = amt;
+        this.trans_num = trans_num;
+        this.customer_id = customer_id;
+        this.city = city;
+        this.state = state;
+        this.city_population = city_population;
+        this.merchant = merchant;
+        this.category = category;
+        this.first = first;
+        this.last = last;
+        this.gender = gender;
+        this.job = job;
+        this.dob = dob;
+    }
+
+    public String gender;
+        @Field("Job")
+        public String job;
+
+    @Override
+    public String toString() {
+        return "Transactions{" +
+                "_id=" + _id +
+                ", trans_date_trans_time='" + trans_date_trans_time + '\'' +
+                ", amt=" + amt +
+                ", trans_num=" + trans_num +
+                ", customer_id=" + customer_id +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", city_population=" + city_population +
+                ", merchant='" + merchant + '\'' +
+                ", category='" + category + '\'' +
+                ", first='" + first + '\'' +
+                ", last='" + last + '\'' +
+                ", gender='" + gender + '\'' +
+                ", job='" + job + '\'' +
+                ", dob='" + dob + '\'' +
+                '}';
+    }
+
+    public String dob;
+    }
+
+
