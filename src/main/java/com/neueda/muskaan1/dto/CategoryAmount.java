4c220df2a1a86ffe20845e9eebb30e310455fa75
Pratyaksha Goswami <goswamipratyaksha02@gmail.com>
2023-08-14 15:59:58 +0000

    updated

--- src/main/java/com/neueda/muskaan1/dto/CategoryAmount.java
+++ src/main/java/com/neueda/muskaan1/dto/CategoryAmount.java
@@ -2,6 +2,23 @@ package com.neueda.muskaan1.dto;
 
 public class CategoryAmount {
     private String category;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getTotal_amt() {
+        return total_amt;
+    }
+
+    public void setTotal_amt(double total_amt) {
+        this.total_amt = total_amt;
+    }
+
     private double total_amt;
 
 }
