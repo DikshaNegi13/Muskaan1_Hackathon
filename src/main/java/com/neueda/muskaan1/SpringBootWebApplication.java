4eaac42e5c108617cad67b45e222d3406ee88c7d
Pratyaksha Goswami <goswamipratyaksha02@gmail.com>
2023-08-17 09:49:46 +0000

    updated

--- src/main/java/com/neueda/muskaan1/SpringBootWebApplication.java
+++ src/main/java/com/neueda/muskaan1/SpringBootWebApplication.java
@@ -1,6 +1,6 @@
 package com.neueda.muskaan1;
 
-import com.neueda.muskaan1.entity.Customer;
+
 import com.neueda.muskaan1.dao.ICustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -8,10 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 import java.util.logging.Logger;
 
-import static org.slf4j.LoggerFactory.getLogger;
 
 @SpringBootApplication
 
